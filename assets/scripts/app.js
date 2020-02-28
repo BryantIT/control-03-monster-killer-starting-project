c4d7812b95c4b74b7c8eb33bde668fc123854ff1
Bryant Richards <dragonseyeimages@gmail.com>
2020-02-28 07:17:08 -0600

    Validating user input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,13 @@ const MONSTER_ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 17;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt('Max life for you and the monster', '100');
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0){
+  chosenMaxLife = 100;
+}
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
@@ -37,7 +43,7 @@ function endRound(){
     alert('You have a draw!');
   }
 
-  if (currentPlayerHealth <= 0 || currentPlayerHealth <= 0){
+  if (currentPlayerHealth <= 0 || currentMonsterHealth <= 0){
     reset()
   }
 }
