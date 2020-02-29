1a7f0ae2fd0427289037dde63d8c617c89baa1e9
Bryant Richards <dragonseyeimages@gmail.com>
2020-02-29 04:56:14 -0600

    switch-case

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -32,16 +32,26 @@ function writeToLog(event, value, monsterHealth, playerHealth){
       finalMonsterHealth: monsterHealth,
       finalPlayerHealth: playerHealth
   }
-  if (event === LOG_EVENT_PLAYER_ATTACK){
-    logEntry.target = 'MONSTER'
-  } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK){
-    logEntry.target = 'MONSTER'
-  } else if (event === LOG_EVENT_MONSTER_ATTACK){
-    logEntry.target = 'PLAYER'
-  } else if (event = LOG_EVENT_PLAYER_HEAL){
-    logEntry.target = 'PLAYER'
-  } else if (event === LOG_EVENT_GAME_OVER){
+  switch (event) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = 'MONSTER'
+    break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = 'MONSTER'
+    break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry.target = 'PLAYER'
+    break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry.target = 'PLAYER'
+    break;
+    case LOG_EVENT_GAME_OVER:
+    break;
+
+  default:
+  logEntry = {};
   }
+
   battleLog.push(logEntry)
 }
 
